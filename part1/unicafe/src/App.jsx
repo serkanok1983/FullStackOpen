cad596f2990509548366e040b2fe9977792fa244
serkanok1983 <serkanok1983@yahoo.com>
2024-12-15 11:54:38 +0300

    1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,20 +5,33 @@ function App() {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const [total, setTotal] = useState(0);
+    const [count, setCount] = useState(0);
 
     const handleGood = () => {
         const updatedGood = good + 1;
         setGood(updatedGood);
+        const updatedCount = count + 1;
+        setCount(updatedCount);
+        const updatedTotal = total + 1;
+        setTotal(updatedTotal);
     }
 
     const handleNeutral = () => {
         const updateNeutral = neutral + 1;
         setNeutral(updateNeutral);
+        const updatedCount = count + 1;
+        setCount(updatedCount);
+        setTotal(total);
     }
 
     const handleBad = () => {
         const updateBad = bad + 1;
         setBad(updateBad);
+        const updatedCount = count + 1;
+        setCount(updatedCount);
+        const updatedTotal = total - 1;
+        setTotal(updatedTotal);
     }
 
     return (
@@ -34,6 +47,9 @@ function App() {
                 <p>good {good}</p>
                 <p>neutral {neutral}</p>
                 <p>bad {bad}</p>
+                <p>all {count}</p>
+                <p>average {total / count}</p>
+                <p>positive {good / count * 100} %</p>
             </div>
         </>
     )
